45e090ca3c5a40032e366315016ffcd18729b36f
Calum Ferris <calum.ferris@gmail.com>
2023-11-08 14:09:57 +0000

    stable; rationalised MRE

--- App.js
+++ App.js
@@ -1,43 +1,12 @@
 import React from "react";
-import { NavigationContainer } from "@react-navigation/native";
-import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { StyleSheet } from "react-native";
 import { GestureHandlerRootView } from "react-native-gesture-handler";
 import MainScreen from "./src/MainScreen";
-import SecondaryScreen from "./src/SecondaryScreen";
-import { StyleSheet } from "react-native";
-import { LinePathProvider } from "./src/LinePathContext";
-
-const Stack = createNativeStackNavigator();
-
-const AppNavigator = () => {
-  return (
-    <Stack.Navigator initialRouteName="Main">
-      <Stack.Screen
-        name="Main"
-        component={MainScreen}
-        options={{
-          headerShown: false,
-        }}
-      />
-      <Stack.Screen
-        name="Secondary"
-        component={SecondaryScreen}
-        options={{
-          headerShown: false,
-        }}
-      />
-    </Stack.Navigator>
-  );
-};
 
 export default function App() {
   return (
     <GestureHandlerRootView style={styles.container}>
-      <LinePathProvider>
-        <NavigationContainer>
-          <AppNavigator />
-        </NavigationContainer>
-      </LinePathProvider>
+      <MainScreen />
     </GestureHandlerRootView>
   );
 }
--- src/MainScreen.js
+++ src/MainScreen.js
@@ -1,23 +1,99 @@
-import React from "react";
-import { Button, StyleSheet, View } from "react-native";
-import SkiaImage from "./SkiaImage";
-import { SkiaView, useCanvasRef } from "@shopify/react-native-skia";
-import { useLinePathContext } from "./LinePathContext";
+import React, { useCallback, useReducer } from "react";
+import { Button, Dimensions, StyleSheet, View } from "react-native";
+import { Skia, rect, useCanvasRef, useImage } from "@shopify/react-native-skia";
+import { useSharedValue } from "react-native-reanimated";
+import GestureHandler from "./GestureHandler";
+import { Canvas, Path, Group, Image } from "@shopify/react-native-skia";
 
-const MainScreen = ({ navigation }) => {
-  const { clearLinePaths, getSnapshots, linePaths } = useLinePathContext();
-  const canvasRef = useCanvasRef();
-  console.log("After invocation of useCanvasRef: ", canvasRef.current !== null);
+const { width, height } = Dimensions.get("window");
+
+const LinePathReducer = (state, action) => {
+  switch (action.type) {
+    case "add":
+      return { ...state, linePaths: [...state.linePaths, action.payload] };
+    case "clear":
+      return { ...state, linePaths: [], snapshots: [] };
+    case "add-snapshots":
+      return { ...state, snapshots: [...action.payload] };
+    default:
+      return state;
+  }
+};
+
+const MainScreen = () => {
+  const initialState = { linePaths: [], snapshots: [] };
+  const [state, dispatch] = useReducer(LinePathReducer, initialState);
+
+  const addLinePath = useCallback(
+    linePath => {
+      dispatch({ type: "add", payload: linePath });
+    },
+    [dispatch],
+  );
+
+  const clearLinePaths = useCallback(() => {
+    dispatch({ type: "clear" });
+  }, [dispatch]);
+
+  const getSnapshots = ({ canvasRef }) => {
+    try {
+      if (state.linePaths.length > 0) {
+        const bounds = state.linePaths.map(linePath =>
+          linePath.path.getBounds(),
+        );
+        const snapshots = bounds
+          .map(bound => {
+            const snapshot = canvasRef.current?.makeImageSnapshot(bound);
+            return snapshot;
+          })
+          .filter(Boolean);
+        console.log("number of snapshots taken= ", snapshots.length);
+        dispatch({ type: "add-snapshots", payload: snapshots });
+      }
+    } catch (err) {
+      console.error(err);
+    }
+  };
+
   const image = require("./zurich.jpg");
+  const skImage = useImage(image);
+  const canvasRef = useCanvasRef();
+  const pictureMatrix = useSharedValue(Skia.Matrix());
+  const pictureDimensions = rect(0, 0, width, height);
   return (
     <>
-      <SkiaView style={styles.container}>
-        <SkiaImage
-          style={styles.skiaImageContainer}
-          imageUri={image}
-          canvasRef={canvasRef}
+      <View style={styles.skiaImageContainer}>
+        <Canvas
+          ref={canvasRef}
+          style={{
+            width,
+            height,
+          }}>
+          <Group matrix={pictureMatrix}>
+            <Image
+              x={0}
+              y={0}
+              width={width}
+              height={height}
+              image={skImage}
+              fit="contain"
+            />
+            {state.linePaths.map((linePath, index) => (
+              <Path
+                key={`${index}-path`}
+                path={linePath.path}
+                paint={linePath.paint}
+              />
+            ))}
+          </Group>
+        </Canvas>
+        <GestureHandler
+          matrix={pictureMatrix}
+          dimensions={pictureDimensions}
+          debug={false}
+          addLinePath={addLinePath}
         />
-      </SkiaView>
+      </View>
       <View style={styles.buttonContainer}>
         <Button
           title="Clear"
@@ -29,18 +105,12 @@ const MainScreen = ({ navigation }) => {
         <Button
           title="Snapshot"
           onPress={() => {
-            console.log("Snapshot!", canvasRef.current);
             try {
-              console.log("inside try block", linePaths.length);
               getSnapshots({ canvasRef });
             } catch (e) {
               console.log(e);
             }
           }}
-        />
-        <Button
-          title="Secondary"
-          onPress={() => navigation.navigate("Secondary")}
         />
       </View>
     </>
@@ -48,13 +118,12 @@ const MainScreen = ({ navigation }) => {
 };
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 92,
-  },
   skiaImageContainer: {
     zIndex: 1,
+    flex: 92,
   },
   buttonContainer: {
+    zIndex: 2,
     flex: 8,
     justifyContent: "space-around",
     alignItems: "center",
